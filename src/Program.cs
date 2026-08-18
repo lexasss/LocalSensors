6ac93e9ddc8e5807875fa678c632da43e6088273
lexasss <oleg.spakov@tuni.fi>
2026-08-18 17:18:28 +0300

    Some sensor info is printed out

--- src/Program.cs
+++ src/Program.cs
@@ -11,41 +11,92 @@ class Program
         foreach (var sensor in provider.Sensors)
             Console.WriteLine($"[{(sensor.IsAvailable ? '+' : '-')}] {sensor.Name}");
 
-        /*
-        var accelerometer = provider.Accelerometer;
-        if (accelerometer == null)
+        Console.WriteLine("\nPress Enter to exit.");
+        var top = Console.CursorTop;
+
+        if (provider.Activity != null)
         {
-            Console.WriteLine("No accelerometer detected.");
-            return;
+            provider.Activity.ReadingChanged += Activity_ReadingChanged;
+            var task = provider.Activity.GetCurrentReadingAsync();
+            task.Wait();
+            var data = task.GetResults();
+            var info = $"{data.Activity}";
+            PrintAt(70, 13, "Activity", info.PadRight(15));
         }
+        if (provider.Accelerometer != null)
+            provider.Accelerometer.ReadingChanged += Accelerometer_ReadingChanged;
+        if (provider.Gyrometer != null)
+            provider.Gyrometer.ReadingChanged += Gyrometer_ReadingChanged;
+        if (provider.Inclinometer != null)
+            provider.Inclinometer.ReadingChanged += Inclinometer_ReadingChanged;
+        if (provider.Orientation != null)
+            provider.Orientation.ReadingChanged += Orientation_ReadingChanged;
+        if (provider.SimpleOrientation != null)
+            provider.SimpleOrientation.OrientationChanged += SimpleOrientation_OrientationChanged;
 
-        Console.WriteLine("Accelerometer found!");
-        Console.WriteLine($"Minimum interval: {accelerometer.MinimumReportInterval} ms");
-
-        accelerometer.ReportInterval =
-            Math.Max(20u, accelerometer.MinimumReportInterval);
-
-        accelerometer.ReadingChanged += Accelerometer_ReadingChanged;
-
-        Console.WriteLine("Reading accelerometer...");
-        */
-
-        Console.WriteLine("Press Enter to exit.");
-
+        Console.CursorVisible = false;
         Console.ReadLine();
+
+        Console.CursorTop = top;
+    }
+
+    private static void Activity_ReadingChanged(ActivitySensor sender, ActivitySensorReadingChangedEventArgs args)
+    {
+        var data = args.Reading;
+        var info = $"X = {data.Activity:F3}";
+        PrintAt(70, 13, "Activity", info.PadRight(15));
+    }
+
+    private static void SimpleOrientation_OrientationChanged(SimpleOrientationSensor sender, SimpleOrientationSensorOrientationChangedEventArgs args)
+    {
+        var data = args.Orientation;
+        var info = $"{data}";
+        PrintAt(50, 13, "Mode", info.PadRight(15));
+    }
+
+    private static void Gyrometer_ReadingChanged(Gyrometer sender, GyrometerReadingChangedEventArgs args)
+    {
+        var data = args.Reading;
+        var info = $"X = {data.AngularVelocityX:F3}°/s, Y = {data.AngularVelocityY:F3}°/s, Z = {data.AngularVelocityY:F3}°/s";
+        PrintAt(50, 10, "Gyrometer", info.PadRight(45));
+    }
+
+    private static void Orientation_ReadingChanged(OrientationSensor sender, OrientationSensorReadingChangedEventArgs args)
+    {
+        var data = args.Reading;
+        var info = $"W = {data.Quaternion.W:F3}, X = {data.Quaternion.X:F3}, Y = {data.Quaternion.Y:F3}, Z = {data.Quaternion.Z:F3}";
+        PrintAt(50, 7, "Orientation", info.PadRight(45));
+    }
+
+    private static void Inclinometer_ReadingChanged(Inclinometer sender, InclinometerReadingChangedEventArgs args)
+    {
+        var data = args.Reading;
+        var info = $"P = {data.PitchDegrees:F3}°, R = {data.RollDegrees:F3}°, Y = {data.YawDegrees:F3}°";
+        PrintAt(50, 4, "Inclinometer", info.PadRight(40));
     }
 
     private static void Accelerometer_ReadingChanged(
         Accelerometer sender,
         AccelerometerReadingChangedEventArgs args)
     {
-        var reading = args.Reading;
-        var info = $"X={reading.AccelerationX:F3} G, " +
-            $"Y={reading.AccelerationY:F3} G, " +
-            $"Z={reading.AccelerationZ:F3} G";
-        info.PadRight(80);
-        
-        Console.CursorLeft = 0;
+        var data = args.Reading;
+        var info = $"X = {data.AccelerationX:F3} G, Y = {data.AccelerationY:F3} G, Z = {data.AccelerationZ:F3} G";
+        PrintAt(50, 1, "Acelerometer", info.PadRight(40));
+    }
+
+    readonly static Lock _locker = new();
+
+    private static void PrintAt(int left, int top, string name, string info)
+    {
+        _locker.Enter();
+
+        Console.CursorLeft = left;
+        Console.CursorTop = top;
+        Console.Write(name);
+        Console.CursorLeft = left;
+        Console.CursorTop = top + 1;
         Console.Write(info);
+
+        _locker.Exit();
     }
 }
