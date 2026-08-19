83bdfd2924bf00ee1d0ea39a49da6cb205b443e6
lexasss <oleg.spakov@tuni.fi>
2026-08-19 15:53:28 +0300

    SensorWpf

---
+++ SensorsWpf/MainViewModel.cs
@@ -0,0 +1,162 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Sensors;
+using Windows.Devices.Sensors;
+
+namespace SensorsWpf;
+
+internal partial class SensorData(string name, string info = "") : ObservableObject
+{
+    [ObservableProperty]
+    public partial string Name { get; set; } = name;
+    [ObservableProperty]
+    public partial string Info { get; set; } = info;
+}
+
+internal partial class MainViewModel : ObservableObject
+{
+    public SensorData Activity { get; } = new("Activity");
+    public SensorData Accelerometer { get; } = new("Accelerometer");
+    public SensorData Altimeter { get; } = new("Altimeter");
+    public SensorData Barometer { get; } = new("Barometer");
+    public SensorData Compass { get; } = new("Compass");
+    public SensorData Gyrometer { get; } = new("Gyrometer");
+    public SensorData HingeAngle { get; } = new("Hinge Angle");
+    public SensorData HumanPresence { get; } = new("Human Presence");
+    public SensorData Inclinometer { get; } = new("Inclinometer");
+    public SensorData Light { get; } = new("Light");
+    public SensorData Magnetometer { get; } = new("Magnetometer");
+    public SensorData Orientation { get; } = new("Orientation");
+    public SensorData Pedometer { get; } = new("Pedometer");
+    public SensorData Proximity { get; } = new("Proximity");
+    public SensorData SimpleOrientation { get; } = new("Mode");
+
+    public MainViewModel()
+    {
+        if (_sensors.Activity != null)
+            _sensors.Activity.ReadingChanged += Activity_ReadingChanged;
+        if (_sensors.Accelerometer != null)
+            _sensors.Accelerometer.ReadingChanged += Accelerometer_ReadingChanged;
+        if (_sensors.Altimeter != null)
+            _sensors.Altimeter.ReadingChanged += Altimeter_ReadingChanged;
+        if (_sensors.Barometer != null)
+            _sensors.Barometer.ReadingChanged += Barometer_ReadingChanged;
+        if (_sensors.Compass != null)
+            _sensors.Compass.ReadingChanged += Compass_ReadingChanged;
+        if (_sensors.Gyrometer != null)
+            _sensors.Gyrometer.ReadingChanged += Gyrometer_ReadingChanged;
+        if (_sensors.HingeAngle != null)
+            _sensors.HingeAngle.ReadingChanged += HingeAngle_ReadingChanged;
+        if (_sensors.HumanPresence != null)
+            _sensors.HumanPresence.ReadingChanged += HumanPresence_ReadingChanged;
+        if (_sensors.Inclinometer != null)
+            _sensors.Inclinometer.ReadingChanged += Inclinometer_ReadingChanged;
+        if (_sensors.Light != null)
+            _sensors.Light.ReadingChanged += Light_ReadingChanged;
+        if (_sensors.Magnetometer != null)
+            _sensors.Magnetometer.ReadingChanged += Magnetometer_ReadingChanged;
+        if (_sensors.Orientation != null)
+            _sensors.Orientation.ReadingChanged += Orientation_ReadingChanged;
+        if (_sensors.Pedometer != null)
+            _sensors.Pedometer.ReadingChanged += Pedometer_ReadingChanged;
+        if (_sensors.Proximity != null)
+            _sensors.Proximity.ReadingChanged += Proximity_ReadingChanged;
+        if (_sensors.SimpleOrientation != null)
+            _sensors.SimpleOrientation.OrientationChanged += SimpleOrientation_OrientationChanged;
+    }
+
+    #region Internal
+
+    SensorProvider _sensors = new();
+
+    private void SimpleOrientation_OrientationChanged(SimpleOrientationSensor sender, SimpleOrientationSensorOrientationChangedEventArgs args)
+    {
+        var data = args.Orientation;
+        SimpleOrientation.Info = $"{data}";
+    }
+
+    private void Activity_ReadingChanged(ActivitySensor sender, ActivitySensorReadingChangedEventArgs args)
+    {
+        var data = args.Reading;
+        Activity.Info = $"{data.Activity:F3}";
+    }
+
+    private void Compass_ReadingChanged(Compass sender, CompassReadingChangedEventArgs args)
+    {
+        var data = args.Reading;
+        Compass.Info = $"{data.HeadingTrueNorth:F3}°";
+    }
+
+    private void HingeAngle_ReadingChanged(HingeAngleSensor sender, HingeAngleSensorReadingChangedEventArgs args)
+    {
+        var data = args.Reading;
+        HingeAngle.Info = $"{data.AngleInDegrees:F3}°";
+    }
+
+    private void Accelerometer_ReadingChanged(Accelerometer sender, AccelerometerReadingChangedEventArgs args)
+    {
+        var data = args.Reading;
+        Accelerometer.Info = $"X = {data.AccelerationX:F3} G, Y = {data.AccelerationY:F3} G, Z = {data.AccelerationZ:F3} G";
+    }
+
+    private void Gyrometer_ReadingChanged(Gyrometer sender, GyrometerReadingChangedEventArgs args)
+    {
+        var data = args.Reading;
+        Gyrometer.Info = $"X = {data.AngularVelocityX:F3}°/s, Y = {data.AngularVelocityY:F3}°/s, Z = {data.AngularVelocityY:F3}°/s";
+    }
+
+    private void Inclinometer_ReadingChanged(Inclinometer sender, InclinometerReadingChangedEventArgs args)
+    {
+        var data = args.Reading;
+        Inclinometer.Info = $"P = {data.PitchDegrees:F3}°, R = {data.RollDegrees:F3}°, Y = {data.YawDegrees:F3}°";
+    }
+
+    private void Orientation_ReadingChanged(OrientationSensor sender, OrientationSensorReadingChangedEventArgs args)
+    {
+        var data = args.Reading;
+        Orientation.Info = $"W = {data.Quaternion.W:F3}, X = {data.Quaternion.X:F3}, Y = {data.Quaternion.Y:F3}, Z = {data.Quaternion.Z:F3}";
+    }
+
+    private void Magnetometer_ReadingChanged(Magnetometer sender, MagnetometerReadingChangedEventArgs args)
+    {
+        var data = args.Reading;
+        Magnetometer.Info = $"X = {data.MagneticFieldX:F3}, Y = {data.MagneticFieldY:F3}, Z = {data.MagneticFieldZ:F3}";
+    }
+
+    private void Pedometer_ReadingChanged(Pedometer sender, PedometerReadingChangedEventArgs args)
+    {
+        var data = args.Reading;
+        Pedometer.Info = $"{data.CumulativeStepsDuration}: {data.CumulativeSteps} ({data.StepKind})";
+    }
+
+    private void HumanPresence_ReadingChanged(HumanPresenceSensor sender, HumanPresenceSensorReadingChangedEventArgs args)
+    {
+        var data = args.Reading;
+        HumanPresence.Info = $"{data.Engagement}: {data.Presence} at {data.DistanceInMillimeters} mm";
+    }
+
+    private void Altimeter_ReadingChanged(Altimeter sender, AltimeterReadingChangedEventArgs args)
+    {
+        var data = args.Reading;
+        Altimeter.Info = $"{data.AltitudeChangeInMeters:F3} m";
+    }
+
+    private void Barometer_ReadingChanged(Barometer sender, BarometerReadingChangedEventArgs args)
+    {
+        var data = args.Reading;
+        Barometer.Info = $"{data.StationPressureInHectopascals:F3} hPa";
+    }
+
+    private void Light_ReadingChanged(LightSensor sender, LightSensorReadingChangedEventArgs args)
+    {
+        var data = args.Reading;
+        Light.Info = $"{data.IlluminanceInLux:F3}°";
+    }
+
+    private void Proximity_ReadingChanged(ProximitySensor sender, ProximitySensorReadingChangedEventArgs args)
+    {
+        var data = args.Reading;
+        Proximity.Info = data.IsDetected ? $"{data.DistanceInMillimeters:F3} mm" : "-";
+    }
+
+    #endregion
+}
---
+++ SensorsWpf/SensorProvider.cs
@@ -0,0 +1,170 @@
+﻿using System.Reflection;
+using Windows.Devices.Sensors;
+using Windows.Foundation;
+
+namespace Sensors;
+
+[AttributeUsage(AttributeTargets.Property)]
+internal class SensorAttribute(string name) : Attribute
+{
+    public string Name { get; } = name;
+}
+
+internal class SensorProvider
+{
+    [Sensor("Activity")]
+    public ActivitySensor? Activity { get; }
+    [Sensor("Accelerometer")]
+    public Accelerometer? Accelerometer { get; }
+    [Sensor("Altimeter")]
+    public Altimeter? Altimeter { get; }
+    [Sensor("Barometer")]
+    public Barometer? Barometer { get; }
+    [Sensor("Compass")]
+    public Compass? Compass { get; }
+    [Sensor("Gyrometer")]
+    public Gyrometer? Gyrometer { get; }
+    [Sensor("HingeAngle")]
+    public HingeAngleSensor? HingeAngle { get; }
+    [Sensor("HumanPresence")]
+    public HumanPresenceSensor? HumanPresence { get; }
+    [Sensor("Inclinometer")]
+    public Inclinometer? Inclinometer { get; }
+    [Sensor("Light")]
+    public LightSensor? Light { get; }
+    [Sensor("Magnetometer")]
+    public Magnetometer? Magnetometer { get; }
+    [Sensor("Orientation")]
+    public OrientationSensor? Orientation { get; }
+    [Sensor("Pedometer")]
+    public Pedometer? Pedometer { get; }
+    [Sensor("Proximity")]
+    public ProximitySensor? Proximity { get; }
+    [Sensor("SimpleOrientation")]
+    public SimpleOrientationSensor? SimpleOrientation { get; }
+
+    public record SensorStatus(string Name, bool IsAvailable);
+
+    public SensorStatus[] Sensors => typeof(SensorProvider)
+        .GetProperties()
+        .Select(p => new
+        {
+            Property = p,
+            Attribute = p.GetCustomAttribute<SensorAttribute>()
+        })
+        .Where(x => x.Attribute != null)
+        .Select(x => new SensorStatus(x.Attribute!.Name, x.Property.GetValue(this) != null))
+        .ToArray();
+
+
+    public SensorProvider()
+    {
+        static T? GetSync<T>()
+        {
+            var method = typeof(T).GetMethod("GetDefaultAsync", BindingFlags.Public | BindingFlags.Static);
+            if (method == null)
+            {
+                return default(T);
+            }
+            try
+            {
+                var task = (IAsyncOperation<T>?)method.Invoke(null, null);
+                task.Wait();
+                return task!.GetResults();
+            }
+            catch
+            {
+                return default(T);
+            }
+        }
+
+        Activity = GetSync<ActivitySensor>();
+        Accelerometer = Accelerometer.GetDefault();
+        Altimeter = Altimeter.GetDefault();
+        Barometer = Barometer.GetDefault();
+        Compass = Compass.GetDefault();
+        Gyrometer = Gyrometer.GetDefault();
+        HingeAngle = GetSync<HingeAngleSensor>();
+        HumanPresence = HumanPresenceSensor.GetDefault();
+        Inclinometer = Inclinometer.GetDefault();
+        Light = LightSensor.GetDefault();
+        Magnetometer = Magnetometer.GetDefault();
+        Orientation = OrientationSensor.GetDefault();
+        Pedometer = GetSync<Pedometer>();
+        SimpleOrientation = SimpleOrientationSensor.GetDefault();
+
+        Proximity = null;
+    }
+
+    /*
+    public void Print()
+    {
+        string[] ListDevices(string? selector, string? pattern = null)
+        {
+            List<string> ids = [];
+
+            var task = selector == null
+                ? DeviceInformation.FindAllAsync()
+                : DeviceInformation.FindAllAsync(selector);
+            task.Wait();
+            var collection = task.GetResults();
+            foreach (var item in collection)
+            {
+                if (!item.Name.StartsWith("Sound") &&
+                    !item.Name.StartsWith("WKS") &&
+                    !item.Name.StartsWith("Oleg") &&
+                    !item.Name.StartsWith("ACX"))
+                    Console.WriteLine($"{item.Name}  [{item.IsDefault}]");
+                if (pattern != null && item.Name.Contains(pattern))
+                {
+                    Console.WriteLine(item.Id);
+                    ids.Add(item.Id);
+                    foreach (var prop in item.Properties)
+                        Console.WriteLine($"  {prop}");
+                }
+            }
+
+            return ids.ToArray();
+        }
+
+        ListDevices(ProximitySensor.GetDeviceSelector());
+        ListDevices(Accelerometer.GetDeviceSelector(AccelerometerReadingType.Standard));
+        ListDevices(ActivitySensor.GetDeviceSelector());
+        ListDevices(Barometer.GetDeviceSelector());
+        ListDevices(Compass.GetDeviceSelector());
+        ListDevices(Gyrometer.GetDeviceSelector());
+        ListDevices(Inclinometer.GetDeviceSelector(SensorReadingType.Absolute));
+        ListDevices(LightSensor.GetDeviceSelector());
+        ListDevices(Magnetometer.GetDeviceSelector());
+        ListDevices(Pedometer.GetDeviceSelector());
+
+        Task.Run(async () =>
+        {
+            string aqs = SerialDevice.GetDeviceSelector("COM4");
+            var dis = await DeviceInformation.FindAllAsync(aqs);
+            if (dis.Count > 0)
+            {
+                DeviceInformation uart = dis[0];
+                Console.WriteLine($"{uart.Id}");
+
+                try
+                {
+                    var serialPort = await SerialDevice.FromIdAsync(uart.Id);
+                } catch { }
+            }
+
+            var ids = ListDevices(null, "Accel");
+            foreach (var id in ids)
+            {
+                try
+                {
+                    var acc = Accelerometer.FromIdAsync(id);
+                }
+                catch
+                {
+                    Console.WriteLine($"Accelerometer {id} cannot be created");
+                }
+            }
+        }).Wait();
+    }*/
+    }
